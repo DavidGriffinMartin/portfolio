d2a2c0de2cd6fd20914778c6c5a5b8ca4effe280
Dave Martin <105015655+DavidGriffinMartin@users.noreply.github.com>
2022-08-02 10:58:04 -0400

    wip: Adds about/projects/resume paths.

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('projects/', views.projects, name='projects'),
+    path('resume/', views.resume, name='resume'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def resume(request):
+    return render(request, 'resume.html')
